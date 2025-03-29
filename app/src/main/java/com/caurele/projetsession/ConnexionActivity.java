4632bacea2080a16d0ca50ba4d8e589b71addc74
reliosome <aurele.a.collin@gmail.com>
2025-03-29 14:53:04 -0400

    ConnexionActivity

--- app/src/main/java/com/caurele/projetsession/ConnexionActivity.java
+++ app/src/main/java/com/caurele/projetsession/ConnexionActivity.java
@@ -7,7 +7,11 @@
  *************************************************************/
 package com.caurele.projetsession;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,17 +19,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ConnexionActivity extends AppCompatActivity {
+
+    private EditText eTxtCourriel, eTxtMotPasse;
+    private Button btnConnect, btnInscrire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_connexion);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        eTxtCourriel = findViewById(R.id.eTxtCourriel);
+        eTxtMotPasse = findViewById(R.id.eTxtMotPasse);
+        btnConnect = findViewById(R.id.btnConnecter);
+        btnInscrire = findViewById(R.id.btnInscrire);
+
+        btnInscrire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intention = new Intent(this, Inscription.java);
+                startActivity(intention);
+            }
         });
     }
 }

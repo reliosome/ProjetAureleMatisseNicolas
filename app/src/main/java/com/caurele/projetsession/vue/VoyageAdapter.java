dacc390fd8a7791e3b65c950aa96d9c0714ca6a1
ChickenT-wav <matisse.ruel.1@ens.etsmtl.ca>
2025-04-08 18:10:49 -0400

    Voyage adapter et voyage dao

---
+++ app/src/main/java/com/caurele/projetsession/vue/VoyageAdapter.java
@@ -0,0 +1,70 @@
+package com.caurele.projetsession.vue;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.caurele.projetsession.R;
+import com.caurele.projetsession.vueModel.Voyage;
+
+import com.squareup.picasso.Picasso;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VoyageAdapter extends ArrayAdapter<Voyage> {
+
+    private final Activity context;
+    private List<Voyage> voyages = new ArrayList<>();
+
+    public VoyageAdapter(Activity context) {
+        super(context, 0);
+        this.context = context;
+    }
+
+    public void setVoyages(List<Voyage> voyages) {
+        this.voyages = voyages;
+        clear();
+        addAll(voyages);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return voyages.size();
+    }
+
+    @Override
+    public Voyage getItem(int position) {
+        return voyages.get(position);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Voyage voyage = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_voyage, parent, false);
+        }
+
+        ImageView image = convertView.findViewById(R.id.voyage_image);
+        TextView titre = convertView.findViewById(R.id.voyage_titre);
+        TextView description = convertView.findViewById(R.id.voyage_description);
+        TextView prix = convertView.findViewById(R.id.voyage_prix);
+
+        titre.setText(voyage.getNom_voyage());
+        description.setText(voyage.getDescription());
+        prix.setText(String.format("%.2f $", voyage.getPrix()));
+
+
+        Picasso.get()
+                .load(voyage.getImage_url())
+
+                .into(image);
+
+        return convertView;
+    }
+}

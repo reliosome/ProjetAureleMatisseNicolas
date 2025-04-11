f41100d09661c0c5166ce4dc210a9158352aa83d
ChickenT-wav <matisse.ruel.1@ens.etsmtl.ca>
2025-04-11 15:08:24 -0400

    Details voyage activity  fix

--- app/src/main/java/com/caurele/projetsession/vue/DetailsVoyageActivity.java
+++ app/src/main/java/com/caurele/projetsession/vue/DetailsVoyageActivity.java
@@ -1,61 +1,30 @@
 package com.caurele.projetsession.vue;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.*;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.caurele.projetsession.R;
-import com.caurele.projetsession.vueModel.Voyage;
+import com.caurele.projetsession.model.ReservationsBD;
 
 public class DetailsVoyageActivity extends AppCompatActivity {
-
-    private TextView titre, destination, description, type, duree, prix, placesDispo, prixTotal;
-    private ImageView image;
-    private Spinner dateSpinner;
-    private EditText nombrePlaces;
-    private Button reserver;
-
-    private Voyage voyage;
-    private Voyage.Trip[] trips;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_details_voyage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-        titre = findViewById(R.id.titreVoyage);
-        destination = findViewById(R.id.destinationVoyage);
-        description = findViewById(R.id.descriptionVoyage);
-        type = findViewById(R.id.typeVoyage);
-        duree = findViewById(R.id.dureeVoyage);
-        prix = findViewById(R.id.prixVoyage);
-        placesDispo = findViewById(R.id.placesDisponibles);
-        prixTotal = findViewById(R.id.prixTotal);
-        image = findViewById(R.id.imageVoyage);
-        dateSpinner = findViewById(R.id.spinnerDate);
-        nombrePlaces = findViewById(R.id.editNombrePlaces);
-        reserver = findViewById(R.id.btnReserver);
 
-        voyage = (Voyage) getIntent().getSerializableExtra("voyage");
-
-        if (voyage == null) return;
-
-        titre.setText(voyage.getNom_voyage());
-        destination.setText(voyage.getDestination());
-        description.setText(voyage.getDescription());
-        type.setText(voyage.getType_de_voyage());
-        duree.setText(voyage.getDuree_jours() + " jours");
-        prix.setText(String.format("%.2f $", voyage.getPrix()));
-
-        com.squareup.picasso.Picasso.get()
-                .load(voyage.getImage_url())
-                .into(image);
-
-        trips = voyage.getTrips();
-        String[] dates = new String[trips.length];
-        for (int i = 0; i < trips.length; i++) {
-            dates[i] = trips[i].date;
-        }
-
-    }}
+    }
+}

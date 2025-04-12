9dda8e2a69dc338e7a3d159e8fe2039ea465411d
reliosome <aurele.a.collin@gmail.com>
2025-04-12 15:42:26 -0400

    notes pour ReservationVueModel

--- app/src/main/java/com/caurele/projetsession/vueModel/ReservationVueModel.java
+++ app/src/main/java/com/caurele/projetsession/vueModel/ReservationVueModel.java
@@ -1,10 +1,23 @@
 package com.caurele.projetsession.vueModel;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
 
-public class ReservationVueModel {
+import com.caurele.projetsession.model.DAO.UtilitaireJSON;
+
+public class ReservationVueModel extends ViewModel {
 
     private LiveData<Reservation> reservations;
+
+    private ReservationRepository reservationRepository;
+    // ReservationVueModel pourrait potentiellement remplacer
+    // entièrement ReservationRepository, mais sinon on peut l'avoir ici
+
+    private UtilitaireJSON utilitaireJSON;
+
+    // Fonctions pour sauvegarder et charger les réservations
+
+    // Fonction pour modifier voyages (places dispo quand on réserve)
 
 
 }
